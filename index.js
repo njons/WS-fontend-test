c69c382e228825a12b319dd08234427838214d31
Nathalie Jonsson <jonsson.nathalie@gmail.com>
2019-02-08 21:02:27 +0100

    update folder structure

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { render } from "react-dom";
+import App from "./public/components/App";
 
-const App = () => <h1>Hello World</h1>;
+// const App = () => <h1>Hello World</h1>;
 
 render(<App />, document.getElementById("root"));
